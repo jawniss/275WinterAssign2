65ed69a8b26347e7423856e183eae21df1e0f72c
cmput274 <cmput274@ualberta.ca>
2019-03-07 22:52:05 -0700

    heap.h done no test yet

--- heap.h
+++ heap.h
@@ -14,7 +14,7 @@ public:
   // constructor not required because the only "initialization"
   // is through the constructor of the variable "heap" which is called by default
 
-  // return the minimum element in the heap
+  // return the minimum v in the heap
   std::pair<T, K> min() const;
 
   // insert an item with the given key
@@ -36,10 +36,11 @@ public:
   int size() const;
 
 
-  int parentindex(int i) {
-    return floor((i-1)/2);
-  }
+  int parentindex(int i);
 
+  int leftChildIndex(int i);
+
+  int rightChildIndex(int i);
 
 private:
   // the array holding the heap
@@ -52,6 +53,59 @@ std::pair<T, K> min() const{
   return heap.at(0); // return minimum pair
 }
 
+int parentindex(int i) {
+  return floor((i-1)/2);
+}
+
+int leftChildIndex(int i){
+  return ((2*i) + 1);
+}
+
+int rightChildIndex(int i){
+  return ((2*i) + 2);
+}
+
+void popMin(){
+  if (size() == 0){
+    cout << "heap is empty" <<endl;
+  }else{
+    std::pair< std::pair<long long, long long>, long long> root = min();
+    int lastIndex = size()-1; // last index
+    int rootIndex = 0;
+    int vIndex;
+    int rightIndex;
+    int leftIndex;
+    //bool heapProperty = false;
+    int difference;
+    iter_swap(heap.begin() + rootIndex, heap.begin() + lastIndex);// swap the root with the
+    heap.pop_back(); // pop the last item from the vector
+    std::pair< std::pair<long long, long long>, long long> v = min(); // set v is the root vertex
+    vIndex = v.second;
+    rightIndex = rightChildIndex(vIndex);
+    leftIndex = leftChildIndex(vIndex);
+    // since the heap vector we bring in is already all in order except root we move down the tree
+    // while heapProperty not satisfied
+    //while(heapProperty == false){
+      while ((vIndex > leftIndex) && (vIndex > rightIndex)){
+        difference = leftIndex - rightIndex;
+        if (difference < 0){ // case where left side is smaller
+          // traverse the left side
+          //std::pair< std::pair<long long, long long>, long long> u = heap.at(leftIndex); // child of v with smallest key
+          iter_swap(heap.begin() + vIndex, heap.begin() + leftIndex);// swap item and key between u and v
+          vIndex = leftIndex;
+          rightIndex = rightChildIndex(vIndex);
+          leftIndex = leftChildIndex(vIndex);
+        } else if (difference > 0){ // case where right side is smaller
+          iter_swap(heap.begin() + vIndex, heap.begin() + rightIndex);// swap item and key between u and v
+          vIndex = rightIndex;
+          rightIndex = rightChildIndex(vIndex);
+          leftIndex = leftChildIndex(vIndex);
+        }
+      }
+    //}
+  }
+}
+
 int size() const{
   // get the size of current instance of graph
   int size = heap.size();
@@ -60,18 +114,19 @@ int size() const{
 
 
 void insert(const T& item, const K& key) {
+  /*
   // probably set item stuff elsewhere
   std::pair<long long, long long> item;
   item.first = vertex1; //first vertex key when we add later
   item.second = vectex2; //second vertex key when we add later
+  */
+
+  std::pair< std::pair<long long, long long>, long long> v;// this is v
+  v.first = item; // two vertexes
+  v.second = key; //manhatten distance;
 
 
-  std::pair< std::pair<long long, long long>, long long> element;// this is v
-  element.first = item; // two vertexes
-  element.second = key; //manhatten distance;
-
-
-  heap.pushback(element);// pushing v into vector
+  heap.push_back(v);// pushing v into vector
   int childIndex = size()-1; // subratact one since vectors are indexed from zero
   // need to find the position of v we know initialy the position of v is the size
   int parentIndex = parentindex(childIndex); // position of the parent
@@ -80,23 +135,25 @@ void insert(const T& item, const K& key) {
   //int keyParent = heap.second[parentIndex]
 
   std::pair< std::pair<long long, long long>, long long> pairParent = heap.at(parentIndex);// key of parent
-  int keyParent = pairParent.second
+  int keyParent = pairParent.second;
 
   std::pair< std::pair<long long, long long>, long long> firstPosition = heap.at(0);
+  //std::pair< std::pair<long long, long long>, long long> firstPosition = min(); // could be better
   int rootCost = firstPosition.second;
 
-  //while ((element.second != rootCost) && (element.second < keyParent )){
-  while ((key != rootCost) && (key < keyParent )){
+  //while ((v.second != rootCost) && (v.second < keyParent )){
+  while ((key != rootCost) && (key < keyParent)){
     // swap the items and keys between v and parent(v)
-    iter_swap(heap.begin() + parentIndex, heap.begin() + childIndex;
+    iter_swap(heap.begin() + parentIndex, heap.begin() + childIndex);
     // setting the indexes and setting v to be the parent(v) locations
     childIndex = parentIndex;
-    parentIndex = parentindex(childIndex);
+    parentIndex = parentindex(childIndex); // index of new parent
     pairParent = heap.at(parentIndex);// key of new parent
     keyParent = pairParent.second;
 
-    // recheck who is the root
+    // rechecking who is the root
     firstPosition = heap.at(0);
+    //firstPosition = min(); // could be better
     rootCost = firstPosition.second;
   }
 }
