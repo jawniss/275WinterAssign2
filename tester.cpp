42966baa25103ad1ff5102d5fd6addfd190f653d
cmput274 <cmput274@ualberta.ca>
2019-03-09 19:48:12 -0700

    issue with heap not sorting properly

--- tester.cpp
+++ tester.cpp
@@ -6,68 +6,74 @@ int main() {
     BinaryHeap<int, int> Bheap;
 
     cout << "size before insert: " << Bheap.size() << endl;
-    Bheap.insert(11, 5);
+    Bheap.insert(21, 6);
     cout << "size after insert: "<< Bheap.size() << endl;
     cout << "heap min cost: "<<Bheap.min().second << endl;
     cout << "heap min pair "<<Bheap.min().first << endl;
-    Bheap.insert(34, 8);
+    Bheap.insert(34, 7);
     cout << "size after insert: "<< Bheap.size() << endl;
     cout << "heap min cost: "<<Bheap.min().second << endl;
     cout << "heap min pair "<<Bheap.min().first << endl;
-    Bheap.insert(54, 1);
+    Bheap.insert(54, 12);
     cout << "size after insert: "<< Bheap.size() << endl;
     cout << "heap min cost: "<<Bheap.min().second << endl;
     cout << "heap min pair "<<Bheap.min().first << endl;
-    Bheap.insert(9, 2);
+    Bheap.insert(9, 10);
     cout << "size after insert: "<< Bheap.size() << endl;
     cout << "heap min cost: "<<Bheap.min().second << endl;
     cout << "heap min pair "<<Bheap.min().first << endl;
-    Bheap.insert(21, 3);
+    Bheap.insert(11, 14);
     cout << "size after insert: "<< Bheap.size() << endl;
     cout << "heap min cost: "<<Bheap.min().second << endl;
     cout << "heap min pair "<<Bheap.min().first << endl;
-    Bheap.insert(50, 7);
-    cout << "size after insert: "<< Bheap.size() << endl;
-    cout << "heap min cost: "<<Bheap.min().second << endl;
-    cout << "heap min pair "<<Bheap.min().first << endl;
-    Bheap.insert(38, 6);
-    cout << "size after insert: "<< Bheap.size() << endl;
-    cout << "heap min cost: "<<Bheap.min().second << endl;
-    cout << "heap min pair "<<Bheap.min().first << endl;
-    Bheap.insert(12, 12);
+    Bheap.insert(50, 13);
     cout << "size after insert: "<< Bheap.size() << endl;
     cout << "heap min cost: "<<Bheap.min().second << endl;
     cout << "heap min pair "<<Bheap.min().first << endl;
 
-    Bheap.popMin();
-    cout << "size after popmin: "<< Bheap.size() << endl;
-    cout << "heap min cost: "<<Bheap.min().second << endl;
-    cout << "heap min pair "<<Bheap.min().first << endl;
-    Bheap.popMin();
-    cout << "size after popmin: "<< Bheap.size() << endl;
-    cout << "heap min cost: "<<Bheap.min().second << endl;
-    cout << "heap min pair "<<Bheap.min().first << endl;
-    Bheap.popMin();
-    cout << "size after popmin: "<< Bheap.size() << endl;
-    cout << "heap min cost: "<<Bheap.min().second << endl;
-    cout << "heap min pair "<<Bheap.min().first << endl;
-    Bheap.popMin();
-    cout << "size after popmin: "<< Bheap.size() << endl;
-    cout << "heap min cost: "<<Bheap.min().second << endl;
-    cout << "heap min pair "<<Bheap.min().first << endl;
-    Bheap.popMin();
-    cout << "size after popmin: "<< Bheap.size() << endl;
-    cout << "heap min cost: "<<Bheap.min().second << endl;
-    cout << "heap min pair "<<Bheap.min().first << endl;
-    Bheap.popMin();
-    cout << "size after popmin: "<< Bheap.size() << endl;
-    cout << "heap min cost: "<<Bheap.min().second << endl;
-    cout << "heap min pair "<<Bheap.min().first << endl;
-    Bheap.popMin();
-    cout << "size after popmin: "<< Bheap.size() << endl;
+
+    Bheap.insert(2, 5);
+    cout << "size after insert: "<< Bheap.size() << endl;
     cout << "heap min cost: "<<Bheap.min().second << endl;
     cout << "heap min pair "<<Bheap.min().first << endl;
 
+    Bheap.insert(4,8);
+    cout << "size after insert: "<< Bheap.size() << endl;
+    cout << "heap min cost: "<<Bheap.min().second << endl;
+    cout << "heap min pair "<<Bheap.min().first << endl;
+
+    Bheap.print();
+
+    /*
+    Bheap.popMin();
+    cout << "size after popmin: "<< Bheap.size() << endl;
+    cout << "heap min cost: "<<Bheap.min().second << endl;
+    cout << "heap min pair "<<Bheap.min().first << endl;
+    Bheap.popMin();
+    cout << "size after popmin: "<< Bheap.size() << endl;
+    cout << "heap min cost: "<<Bheap.min().second << endl;
+    cout << "heap min pair "<<Bheap.min().first << endl;
+    Bheap.popMin();
+    cout << "size after popmin: "<< Bheap.size() << endl;
+    cout << "heap min cost: "<<Bheap.min().second << endl;
+    cout << "heap min pair "<<Bheap.min().first << endl;
+    Bheap.popMin();
+    cout << "size after popmin: "<< Bheap.size() << endl;
+    cout << "heap min cost: "<<Bheap.min().second << endl;
+    cout << "heap min pair "<<Bheap.min().first << endl;
+    Bheap.popMin();
+    cout << "size after popmin: "<< Bheap.size() << endl;
+    cout << "heap min cost: "<<Bheap.min().second << endl;
+    cout << "heap min pair "<<Bheap.min().first << endl;
+    Bheap.popMin();
+    cout << "size after popmin: "<< Bheap.size() << endl;
+    cout << "heap min cost: "<<Bheap.min().second << endl;
+    cout << "heap min pair "<<Bheap.min().first << endl;
+    Bheap.popMin();
+    cout << "size after popmin: "<< Bheap.size() << endl;
+    cout << "heap min cost: "<<Bheap.min().second << endl;
+    cout << "heap min pair "<<Bheap.min().first << endl;
+    */
 
     return 0;
 }
